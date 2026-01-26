222b267f3d9f1767523d1e136c4960251bf6fc3b
Cdinetam <cdinetamCdinetam@laptop-von-claudine-1.home>
2026-01-26 19:20:12 +0100

    Update favicon to new TaVyro 20260126 version (PNG)

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon.png', type: 'image/png' },
       { url: '/icon.svg', type: 'image/svg+xml' },
     ],
-    apple: '/icon.svg',
+    apple: '/favicon.png',
   },
 };
 
